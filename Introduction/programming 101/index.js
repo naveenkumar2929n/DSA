d8554d04e15febb0337a3f62900bffe0704679cf
Naveen4272 <naveenkumar4272n@gmail.com>
2025-07-13 02:43:52 +0530

    introduction

---
+++ Introduction/programming 101/index.js
@@ -0,0 +1,20 @@
+
+//Programming 101:
+//types of creating a variable:
+let a=10;//Number
+const b=20;
+var c=30;
+let h="hello world";//String
+var k=true;//boolean
+//console.log(typeof(k));
+//Arrays in javaScript:Index starts from 0
+let arr=["hello",8,true,"virat",[10,20]];
+//console.log(arr[4][1]);
+//objects in javascript: contains key value pairs
+let obj={
+  a:10,
+  name1:"hello",
+  name2:"world",
+  bool:true
+}
+console.log(obj.name1+ " "+obj.name2);
