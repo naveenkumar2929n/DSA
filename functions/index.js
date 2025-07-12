b0ab61808335b5f0fb4000fc4892d5edace45085
Naveen4272 <naveenkumar4272n@gmail.com>
2025-07-13 02:46:20 +0530

    functions

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+function sum(a,b){
+  let sum=a+b;
+ console.log(sum);//function declaration
+ 
+}
+
+
+//sum(100,20);=>function calling
+
+//returning the value instead of printing
+function square( x){
+  let result=x*x;
+  return result;//returns the result
+
+}
+
+let ans=square(4);
+console.log(ans);
