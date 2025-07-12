b45de060edc85e73c3e2a8fb032e75ce68e4aae3
Naveen4272 <naveenkumar4272n@gmail.com>
2025-07-13 03:16:54 +0530

    loops

---
+++ loops/index.js
@@ -0,0 +1,36 @@
+//for loop:
+
+for(let i=1;i<=5;i++){
+  console.log("hello world"+i);
+}
+//i=1 is initalization;
+//i<=5 is condition;
+//i++ or i-- is case(increment or decrement);
+
+
+//while loop:
+
+let x=1;//initialization
+while(x<=5){
+  console.log("hello world1");
+  x++;//case
+}
+
+//do while loop:
+
+let y=1;
+do{
+  console.log("hello world");
+  
+}while(y!=1);
+
+
+
+
+
+
+
+for(let a=5; a>0;a--){
+  console.log(a);
+  
+}
