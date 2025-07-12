329ef887759ba1c3cba25fa08883558c68f6bd75
Naveen4272 <naveenkumar4272n@gmail.com>
2025-07-13 02:48:30 +0530

    conditional statements

---
+++ condtional statements/index.js
@@ -0,0 +1,34 @@
+
+
+
+//if else
+
+  function eligibleAge(age){
+    if(age<0){
+      console.log("invalid input");
+      
+    }else if(age<18){
+      console.log(" your not eligible");
+      
+    }
+    else{
+      console.log("your eligible");
+      
+    }
+  }
+
+
+  //eligibleAge(17);
+
+
+  function isEvenOrOdd(x){
+    if(x%2==0){
+      console.log("even number");
+      
+    }else{
+      console.log("odd number");
+      
+    }
+  }
+  isEvenOrOdd(3);
+  
